5c98433a580db99ea9101937f6ab174709027a44
daylight-star <2919948014@qq.com>
2022-04-10 22:29:38 +0800

    小练习

--- 4.10/4.10/出生日期输入输出.c
+++ 4.10/4.10/出生日期输入输出.c
@@ -1,14 +1,12 @@
-#define _CRT_SECURE_NO_WARNINGS = 1;
+#define _CRT_SECURE_NO_WARNINGS = 1
 #include<stdio.h>
-
-
 int main()
 {
     int year = 0, month = 0, date = 0;
     scanf("%4d%2d%2d", &year, &month, &date);
     printf("year=%04d\n", year);
-            month=%02d\n
-            date=%02d\n", date);
+    printf("month=%02d\n", month);
+    printf("date=%02d\n", date);
 
 
     return 0;
