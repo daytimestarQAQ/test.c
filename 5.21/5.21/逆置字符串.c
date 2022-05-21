a4de7b4f035c2486f791abb86b9fb76b7dcd1105
daylight-star <2919948014@qq.com>
2022-05-21 23:49:59 +0800

    无

---
+++ 5.21/5.21/逆置字符串.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+//标题：倒置字符串 | 时间限制：1秒 | 内存限制：32768K | 语言限制： 不限
+//【倒置字符串】将一句话的单词进行倒置，标点不倒置。
+//比如 I like beijing.经过函数后变为：beijing.like I
+
+
+#include<stdio.h>
+
+int main()
+{
+	char ch[100];
+
+	while((ch = getchar())!='\n')
+	return 0;
+}
